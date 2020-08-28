884b7677f097a84b598ec1ed59340becdbc95e44
nisbc <ext-nischalkumar.bc@here.com>
2020-08-28 17:43:37 +0530

    some more code

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-const express = require('express');
+const path = require('path');
 
-const routes = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/add-product', routes);
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
-app.use('/', (req, res, next) => {
-    console.log('In other middleware');
-    res.send('<h1>hello from express</h1>');
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
