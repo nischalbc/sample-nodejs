fd341eb83c317ab03f43cf9ecf7e667adea9f632
nisbc <ext-nischalkumar.bc@here.com>
2020-08-30 11:08:43 +0530

    some more pages and little more routes registered and controller modified (gave more sense)

--- routes/admin.js
+++ routes/admin.js
@@ -4,14 +4,20 @@ const express = require('express');
 
 const rootDir = require('../util/path');
 
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', productsController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getAdminProducts);
+
+// /admin/edit-product => PUT
+router.put('/edit-product', adminController.putEditProduct);
 
 module.exports = router;
