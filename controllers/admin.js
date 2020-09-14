b8161a7d14a6264fe81dd2db3915386ebc2df5ba
nisbc <ext-nischalkumar.bc@here.com>
2020-09-14 09:42:14 +0530

    removed some unwanted code

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,11 +1,5 @@
 const Product = require('../models/product');
 
-exports.getProducts = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render('admin/products', {prods: products, pageTitle: 'Admin Products', path: '/admin/products'});
-    });
-}
-
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 };
